487bbdbc0f520552122b74dd4e6c85dda1fa8c8b
phantien1002 <phanquangtien1002@gmail.com>
2025-12-17 12:08:43 +0700

    Ingredient calculator

---
+++ RecipeSharing.DAL/Repositories/Interfaces/IRecipeRepository.cs
@@ -0,0 +1,14 @@
+﻿using RecipeSharing.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeSharing.DAL.Repositories.Interfaces
+{
+    public interface IRecipeRepository
+    {
+        Recipe? GetRecipeWithIngredients(int recipeId);
+    }
+}
---
+++ RecipeSharing.DAL/Repositories/RecipeRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RecipeSharing.DAL.Models;
+using RecipeSharing.DAL.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeSharing.DAL.Repositories
+{
+    public class RecipeRepository : IRecipeRepository
+    {
+        private readonly RecipeSharingDbContext _context;
+        public RecipeRepository(RecipeSharingDbContext context)
+        {
+            _context = context;
+        }
+
+        public Recipe? GetRecipeWithIngredients(int recipeId)
+        {
+            return _context.Recipes
+                .Include(r => r.RecipeIngredients)
+                .ThenInclude(ri => ri.Ingredient)
+                .FirstOrDefault(r => r.RecipeId == recipeId);
+        }
+    }
+}
