6046b96cffeaffea2480d70f49b8db2858bada00
tucnzz112 <vohuynhtuandan@gmail.com>
2025-12-18 14:23:37 +0700

    fix

--- RecipeSharing.Pages/Program.cs
+++ RecipeSharing.Pages/Program.cs
@@ -1,12 +1,16 @@
 using Microsoft.EntityFrameworkCore;
 using RecipeSharing.DAL.Models;
+using RecipeSharing.BLL.Interfaces;
+using RecipeSharing.BLL.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
+builder.Services.AddScoped<IShoppingListService, ShoppingListService>();
+
 
 builder.Services.AddDbContext<RecipeSharingDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("RecipeSharingDbConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
